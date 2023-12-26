b7763e439336335f9e01f92fbd11510f21a4ad6a
PDWAWE <32886138+XXX1pdwawe1XXX@users.noreply.github.com>
2023-12-26 11:47:04 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Corporatum_VS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
