1a057a511fb78caf035084514b002a0bf7ce84cf
RobbyInfinger <robby.infinger@gmail.com>
2023-12-26 18:36:14 -0500

    Currn

---
+++ Corporatum/model/CurrencyMC.cs
@@ -0,0 +1,19 @@
+import ICurrency;
+
+class CurrencyMc : ICurrency
+{
+	/*
+	 * Somebody make this stupid piece of shit work with MC
+	 * 
+	 */
+
+	public int getCurrency()
+    {
+		//Get from MC server currency lol
+    }
+
+	public void setCurrency()
+    {
+		//Modify the currency to effect the MC server.
+    }
+}
---
+++ Corporatum/model/ICurrency.cs
@@ -0,0 +1,11 @@
+using System;
+
+
+namespace ICurrency
+{
+	interface ICurrency
+    {
+        public void setCurrency(double);
+        public int getCurrency();
+    }
+}
